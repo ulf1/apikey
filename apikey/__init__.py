a56e634ae459166115f2e1de3e86a03f9efb3f63
UH <554c46@gmail.com>
2021-04-08 12:33:52 +0200

    pkg version variable added

--- apikey/__init__.py
+++ apikey/__init__.py
@@ -1,1 +1,3 @@
+__version__ = '0.2.3'
+
 from .apikey import save, load
--- setup.py
+++ setup.py
@@ -2,8 +2,18 @@ from setuptools import setup
 import pypandoc
 
 
+def get_version(path):
+    with open(path, "r") as fp:
+        lines = fp.read()
+    for line in lines.split("\n"):
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(name='apikey',
-      version='0.2.2',
+      version=get_version("apikey/__init__.py"),
       description='save and load API keys from a file',
       long_description=pypandoc.convert('README.md', 'rst'),
       url='http://github.com/ulf1/apikey',
