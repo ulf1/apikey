404634318a074d7dacb9bcaf59363e62f200dbe6
UH <554c46@gmail.com>
2021-04-27 15:53:49 +0200

    read rst, update license

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup
+import os
+
+
+def read(fname):    
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 def get_version(path):
@@ -14,12 +19,13 @@ def get_version(path):
 setup(name='apikey',
       version=get_version("apikey/__init__.py"),
       description='save and load API keys from a file',
-      long_description='README.rst',
+      long_description=read('README.rst'),
       url='http://github.com/ulf1/apikey',
       author='Ulf Hamster',
       author_email='554c46@gmail.com',
-      license='MIT',
+      license='Apache License 2.0',
       packages=['apikey'],
       # install_requires=[],
       python_requires='>=3.6',
-      zip_safe=True)
+      zip_safe=True
+)
